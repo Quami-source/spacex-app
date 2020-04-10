a6bd7c60f435d8a3e430e47f3d77c6d5914e762b
Quami <quami@gmail.com>
2020-04-10 13:11:33 +0000

    Finished up the app

---
+++ public/precache-manifest.06034cb67e752740223007d9087bc1bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1722abfe98a1192340b147af8f3d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fc1a0779c6a02b802d",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "880ebbb98f8baa38692e",
+    "url": "/static/js/2.2ea11573.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.2ea11573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fc1a0779c6a02b802d",
+    "url": "/static/js/main.2a33af34.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/spacex.7dab9996.jpg"
+  }
+]);
